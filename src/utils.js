0f7e2ab4806635984bbb079df910f3638eff76f4
euhoo <euhoo@mail.ru>
2019-04-03 01:24:43 +0300

    fix frame color

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export const setFrameColor = (st) => {
   let color = 'is-valid';
-  if (st.value === false) color = '';
+  if (st.value.length === 0) color = '';
   else if (st.correctUrl === false) color = 'is-invalid';
   return color;
 };
