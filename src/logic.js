b7aa3ebb024d12b494ea24f31f073d8e20a3aed4
euhoo <euhoo@mail.ru>
2019-04-03 21:01:27 +0300

    better tag making

--- src/logic.js
+++ src/logic.js
@@ -1,14 +1,16 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-param-reassign */
 import WatchJS from 'melanke-watchjs';
 import isURL from 'validator/lib/isURL';
-import axios from 'axios';
-import { cleanClassList, setFrameColor } from './utils';
+import makeRequest from './request';
+import { cleanClassList, setFrameColor, makeTemporyVisible } from './utils';
 
 /*
 поправить
 
 4.разнести все по своим файлам
 5.очистка поля после нажатия
-
+6.добавить по событию нажатию на enter
 
 */
 
@@ -23,7 +25,7 @@ export default () => {
     buttonState: false, // true - отсюда буду брать свойство-активна кнопка или нет
     inputFrame: 'none', // none/red/green-отсюда вотчим рамку
     currentRss: {}, // текущий rss
-    allRss: [],
+    allRss: [], // все ссылки на все rss страницы
   };
 
   /*     controller      */
@@ -32,15 +34,14 @@ export default () => {
   const button = document.querySelector('#main-button');
 
   const buttonStateFunc = () => {
-    // eslint-disable-next-line no-unused-expressions
     button.hasAttribute('disabled') ? button.removeAttribute('disabled') : button.setAttribute('disabled', 'disabled');
   };
 
-  const addingFunc = () => {
+  const makeButtonTemporalBlocked = (delay) => {
     buttonStateFunc();
     window.setTimeout(() => {
       buttonStateFunc();
-    }, 3000);
+    }, delay);
   };
 
 
@@ -52,109 +53,60 @@ export default () => {
   });
 
   button.addEventListener('click', () => {
-    addingFunc();
-    const parser = new DOMParser();
+    makeButtonTemporalBlocked(2000);
     const link = state.value;
-
-    // input.value = '';
-    // state.buttonState = '';
-    // setFrameColor(state);
-    console.log(input);
     const cors = 'https://cors-anywhere.herokuapp.com/';
-    axios.get(`${cors}${link}`)
-      .then(({ data }) => {
-        const doc = parser.parseFromString(data, 'application/xml');
-        const title = doc.querySelector('title').textContent; // посмотреть как точно искать
-        const items = [...doc.querySelectorAll('item')];// посмотреть как точно искать
-        state.currentRss = {
-          title,
-          items,
-          value: state.value,
-        };
-        state.allRss = [state.currentRss, ...state.allRss];
-      })
-
-      .catch(() => {
-        state.error = true;
-      });
+    const url = `${cors}${link}`;
+    makeRequest(url, state);
   });
 
-
-  const errorFunc = () => {
+  const errorStateFunc = () => {
     if (state.error === false) return;
-    const errorDiv = document.querySelector('#error');
-    errorDiv.setAttribute('style', 'display:block;');
-    window.setTimeout(() => {
-      errorDiv.setAttribute('style', 'display:none;');
-    }, 3000);
+    makeTemporyVisible('#error', 3000);
     state.error = false;
   };
 
-  const addFunc = () => {
-    const errorDiv = document.querySelector('#added');
-    errorDiv.setAttribute('style', 'display:block;');
-    window.setTimeout(() => {
-      errorDiv.setAttribute('style', 'display:none;');
-    }, 1000);
-  };
-  const alreadyHaveFunc = () => {
-    const errorDiv = document.querySelector('#have');
-    errorDiv.setAttribute('style', 'display:block;');
-    window.setTimeout(() => {
-      errorDiv.setAttribute('style', 'display:none;');
-    }, 1000);
-  };
-
   /*  view  */
-
 
   const inputFrameFunc = (st) => {
     cleanClassList(input.classList);
     if (st.inputFrame.length > 0) input.classList.add(st.inputFrame);
   };
-
   const renderRss = () => {
-    // есть ли в state.allRss ссылка. если да-return
-    const arr = state.allRss;
-    const filtered = arr.filter(item => item === state.value);
+    const filtered = state.allRss.filter(item => item === state.value);
     if (filtered.length > 0) {
-      alreadyHaveFunc();
+      makeTemporyVisible('#have', 1000);
       return;
     }
+
     state.allRss = [state.value, ...state.allRss];
-    const forRss = document.querySelector('#rss');
     const rssObj = state.currentRss;
-    const rssParser = new DOMParser();
-    const mainDivStr = `
-  <div class="container-fluid w-100">
-  <div class="row no-gutters">
-  <div id="title" class="col-12">
-      <h2>${rssObj.title}</h2>
-  </div>
-  <div id="content" class="col-12">
-  </div>
-</div>
-</div>`;
-    const doc = rssParser.parseFromString(mainDivStr, 'text/html');
-    const top = doc.querySelector('.container-fluid');
-    const divRss = doc.querySelector('#content');
+    const res = [];
     rssObj.items.forEach((item) => {
-      const text = item.querySelector('title').textContent;
-      const href = item.querySelector('link').textContent;
-      const a = document.createElement('a');
-      a.setAttribute('href', href);
-      a.innerHTML = text;
-      const div = document.createElement('li');
-      div.classList.add('col-12');
-      div.append(a);
-      divRss.append(div);
+      const feedStr = `
+      <li class="col-12">
+        <a href="${item.querySelector('link').textContent}">
+          ${item.querySelector('title').textContent}
+        </a>
+      </li>`;
+      res.push(feedStr);
     });
-    addFunc();
-    forRss.appendChild(top);
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <div class="row no-gutters">
+        <div  class="col-12">
+          <h2 id="title">${rssObj.title}</h2>
+        </div>
+        <div class="col-12">
+          ${res.join('')}
+        </div>
+      </div>`;
+    document.querySelector('#rss').appendChild(div);
+    makeTemporyVisible('#added', 1000);
   };
 
   watch(state, 'inputFrame', () => inputFrameFunc(state));
   watch(state, 'buttonState', buttonStateFunc);
-  watch(state, 'error', errorFunc);
+  watch(state, 'error', errorStateFunc);
   watch(state, 'currentRss', renderRss);
 };
---
+++ src/request.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-param-reassign */
+import axios from 'axios';
+
+const parser = new DOMParser();
+
+export default (url, stateObj) => {
+  axios.get(url)
+    .then(({ data }) => {
+      const doc = parser.parseFromString(data, 'application/xml');
+      const title = doc.querySelector('title').textContent;
+      const items = [...doc.querySelectorAll('item')];
+      stateObj.currentRss = {
+        title,
+        items,
+        value: stateObj.value,
+      };
+      stateObj.allRss = [stateObj.currentRss, ...stateObj.allRss];
+    })
+    .catch(() => {
+      stateObj.error = true;
+    });
+};
