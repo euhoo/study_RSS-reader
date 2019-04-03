9e65dde06caec1edcea37e772b6d2580a5e43f80
euhoo <euhoo@mail.ru>
2019-04-04 02:10:06 +0300

    modal trying

---
+++ src/modal.js
@@ -0,0 +1,27 @@
+/* eslint-disable max-len */
+/* const title = item.querySelector('title').textContent;
+      const href = item.querySelector('link').textContent;
+      const id = title.split(' ').join('').substr(5, 12);
+      const clearId = id.replace(/[.,\/#!$%\^&\*;:'’{}=\-_`~()]/g, '');
+      //console.log(result);
+
+
+      const modalWindow = `
+      <div class="modal fade" id="${clearId}" tabindex="-1" role="dialog" aria-labelledby="${clearId}"
+      aria-hidden="true">
+      <div class="modal-dialog" role="document">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="${clearId}Label">${title}</h5>
+                  <button class="close" type="button" data-dismiss="modal" aria-label="close">
+                      <span aria-hidden="true">&times;</span>
+                  </button>
+              </div>
+              <div class="modal-body">
+
+              </div>
+
+          </div>
+      </div>
+  </div>`;
+  */
