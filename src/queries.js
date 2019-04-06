1ce688fd5f7f015389f2afee19412db5fb703dcd
euhoo <euhoo@mail.ru>
2019-04-06 08:58:18 +0300

    make correct updating

--- src/queries.js
+++ src/queries.js
@@ -6,57 +6,56 @@ const parse = (data) => {
   return parser.parseFromString(data, 'application/xml');
 };
 
-const nakeUpdate = (state) => {
-  const links = state.feedLinks;
-  links.forEach((link) => {
-    axios.get(link)
-      .then(({ data }) => {
-        const doc = parse(data);
-        const existTitle = doc.querySelector('title').textContent;
-        let existFeed;
-        state.feeds.forEach((el) => {
-          if (el.title === existTitle) {
-            existFeed = el.items;
-          }
-        });
-        const newFeed = doc.querySelector('item');
-        if (newFeed.querySelector('link').textContent !== existFeed[0].querySelector('link').textContent) {
-          state.newFeed = {
-            channel: existTitle,
-            content: newFeed,
-          };
+const makeUpdate = (state, url, title) => {
+  axios.get(url)
+    .then(({ data }) => {
+      console.log('in');
+      let existFeed;
+      state.feeds.forEach((el) => {
+        if (el.title === title) {
+          existFeed = el.items;
         }
-      })
-      .catch(() => {
-      })
-      .finally(() => {
-        setTimeout(() => {
-          nakeUpdate(state);
-        }, 5000);
       });
-  });
+      const doc = parse(data);
+      const newFeed = doc.querySelector('item');
+      if (newFeed.querySelector('link').textContent !== existFeed[0].querySelector('link').textContent) {
+        existFeed[0].querySelector('link').textContent = newFeed.querySelector('link').textContent;
+        state.newFeed = {
+          channel: title,
+          content: newFeed,
+        };
+      }
+    })
+    .catch(() => {
+    });
 };
 
 // eslint-disable-next-line import/prefer-default-export
 export const getFeed = (url, state) => {
+  let link;
+  let existTitle;
+
   axios.get(url)
     .then(({ data }) => {
       state.process = 'init';
       const doc = parse(data);
       const title = doc.querySelector('title').textContent;
       const items = [...doc.querySelectorAll('item')];
-
       state.feedLinks = [...state.feedLinks, url];
       state.currentFeed = { title, items };
-
       state.feeds = [state.currentFeed, ...state.feeds];
+      link = url;
+      existTitle = title;
     })
     .catch(() => {
       state.process = 'error';
     })
     .finally(() => {
-      setTimeout(() => {
-        nakeUpdate(state);
-      }, 5000);
+      if (link) {
+        console.log('out');
+        setInterval(() => {
+        makeUpdate(state, link, existTitle);
+      }, 2000);
+      }
     });
 };
