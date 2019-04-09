a239748bea84a22a14f9400fa498bbef71726fc9
euhoo <euhoo@mail.ru>
2019-04-09 20:14:19 +0300

    fix step2 again

--- src/queries.js
+++ src/queries.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { difference } from 'lodash';
+import { differenceBy, random } from 'lodash';
 
 const parse = (data) => {
   const parser = new DOMParser();
@@ -7,7 +7,7 @@ const parse = (data) => {
   const title = doc.querySelector('title').textContent;
   const items = [...doc.querySelectorAll('item')];
   const feeds = items.map((item) => {
-    const id = Math.random().toString(36).substr(2, 25);
+    const id = random(1, 1000000);
     const href = item.querySelector('link').textContent;
     const feedTitle = item.querySelector('title').textContent;
     const description = item.querySelector('description').textContent;
@@ -19,24 +19,15 @@ const parse = (data) => {
   return { title, feeds };
 };
 
-const findNewFeeds = (newFeeds, oldFeeds) => {
-  const newFeedsLinks = newFeeds.map(feed => feed.href);
-  const OldFeedsLinks = oldFeeds.map(feed => feed.href);
-  const diff = difference(newFeedsLinks, OldFeedsLinks);
-  return newFeeds.filter(feed => diff.includes(feed.href));
-};
-
 export const updateQuery = (state) => {
   const promises = state.feedLinks.map(link => axios.get(link));
   axios.all(promises)
     .then((allRssChannels) => {
       const newFeeds = allRssChannels.map(channel => parse(channel.data).feeds)
         .flat();
-      const feedsToAdd = findNewFeeds(newFeeds, state.feeds);
-      if (feedsToAdd.length > 0) {
-        // eslint-disable-next-line no-param-reassign
-        state.feeds = [...feedsToAdd, ...state.feeds];
-      }
+      const feedsToAdd = differenceBy(newFeeds, state.feeds, 'href');
+      // eslint-disable-next-line no-param-reassign
+      state.feeds = [...feedsToAdd, ...state.feeds];
     })
     .finally(() => {
       setInterval(() => {
